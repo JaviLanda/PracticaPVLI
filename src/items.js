447f7dec771c66cede0fd05122738b26489eac96
JaviLanda <javlanda@ucm.es>
2016-11-04 00:46:37 +0100

    items modificado

--- src/items.js
+++ src/items.js
@@ -2,16 +2,23 @@
 
 function Item(name, effect) {
   this.name = name;
-  this.effect = effect;
+  this.effect = effect;         
 }
 
-function Weapon(name, damage, extraEffect) {
+function Weapon(name, effect, damage, extraEffect) {
   extraEffect = extraEffect || new Effect({});
+  Item.call(this, name, effect);
+  this.damage = damage;
+  this.extraEffect = extraEffect;
+
   // Haz que Weapon sea subtipo de Item haciendo que llame al constructor de
-  // de Item.
+  // de Item. (HECHO)
 }
+Weapon.prototype = Object.create(Item.prototype);
+Weapon.prototype.constructor = Weapon;
+
 // Termina de implementar la herencia haciendo que la propiedad prototype de
-// Item sea el prototipo de Weapon.prototype y recuerda ajustar el constructor.
+// Item sea el prototipo de Weapon.prototype y recuerda ajustar el constructor. (CREO QUE HECHO)
 
 function Scroll(name, cost, effect) {
   Item.call(this, name, effect);
@@ -23,11 +30,14 @@ Scroll.prototype.constructor = Scroll;
 Scroll.prototype.canBeUsed = function (mp) {
   // El pergamino puede usarse si los puntos de maná son superiores o iguales
   // al coste del hechizo.
+  if(mp >= )
 };
 
 function Effect(variations) {
-  // Copia las propiedades que se encuentran en variations como propiedades de
-  // este objeto.
+   var variations;
+   var effect;
+   // Copia las propiedades que se encuentran en variations como propiedades de
+   // este objeto.(HECHO)
 }
 
 module.exports = {
